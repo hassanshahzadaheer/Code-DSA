d0f06881f2563814d536f807a60ab3a674590f6d
Hassan Shahzad Aheer <hassanshahzadaheer@gmail.com>
2023-06-05 06:39:22 +0500

    TwoSum.java

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return new int[]{};
+    }
+}
