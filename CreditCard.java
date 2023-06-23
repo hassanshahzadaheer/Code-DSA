1cdc9bb41797f4b0724e09c6cb7c82f9334ce755
Hassan Shahzad Aheer <hassanshahzadaheer@gmail.com>
2023-06-23 07:28:23 +0500

    Update CreditCard.java

--- CreditCard.java
+++ CreditCard.java
@@ -1,13 +1,13 @@
 public class CreditCard {
 
-    //
+    // Declare private instance variables
     private String customer;
     private String bank;
     private String account;
     private int limit;
     protected double balance;
 
-    // constructor
+    // Constructor with all parameters
     public CreditCard(String cus, String bnk, String acc, int lim, double blnc)
     {
         customer = cus;
@@ -17,10 +17,12 @@ public class CreditCard {
         balance = blnc;
     }
 
+    // Constructor without balance parameter, sets balance to 0.0
     public CreditCard(String cust, String bk, String acnt, int lim) {
-        this(cus,bnk,acc,lim,0.0);
+        this(cust, bk, acnt, lim, 0.0);
     }
 
+    // Getter methods
     public String getCustomer()
     {
         return customer;
@@ -42,5 +44,15 @@ public class CreditCard {
         return balance;
     }
 
+    public static void main(String[] args) {
+        // Create a CreditCard object
+        CreditCard card = new CreditCard("John Doe", "ABC Bank", "1234567890", 5000, 2500.0);
 
+        // Print the customer details
+        System.out.println("Customer: " + card.getCustomer());
+        System.out.println("Bank: " + card.getBank());
+        System.out.println("Account: " + card.getAccount());
+        System.out.println("Limit: " + card.getLimit());
+        System.out.println("Balance: " + card.getBalance());
+    }
 }
