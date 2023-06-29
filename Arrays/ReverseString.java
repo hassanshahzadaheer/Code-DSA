408f73476ef4f23a24396e2070fb3e19be5ed3bc
Hassan Shahzad Aheer <hassanshahzadaheer@gmail.com>
2023-06-30 04:01:49 +0500

    Update ReverseString.java

--- Arrays/ReverseString.java
+++ Arrays/ReverseString.java
@@ -1,10 +1,13 @@
 import java.util.Arrays;
+
 public class ReverseString {
     public static void main(String args[]) {
 
         String word = "Aheer!";
 
+        // Iterate through each character of the word in reverse order
         for (int i = word.length() - 1; i >= 0; i--) {
+            // Print the character at the current index
             System.out.print(word.charAt(i));
         }
 
